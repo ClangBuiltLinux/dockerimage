5bac707db718bda0f5c659e11e72035654adc990
Nathan Chancellor <natechancellor@gmail.com>
2018-11-18 22:24:43 -0700

    travis: Start tagging by date
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push nathanchance/cbl
+docker push nathanchance/cbl:${BASE}-${TAG}
+docker tag nathanchance/cbl:${BASE}-${TAG} nathanchance/cbl:${BASE}
+[[ ${BASE} = "debian" ]] && docker tag nathanchance/cbl:${BASE}-${TAG} nathanchance/cbl:latest
