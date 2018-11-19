2dbba7fc8b9d3a62a3d31087b9b7bb772356bd39
Nathan Chancellor <natechancellor@gmail.com>
2018-11-18 22:43:19 -0700

    deploy.sh: Fix tagging latest
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -eux
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push nathanchance/cbl:${BASE}-${TAG}
 docker tag nathanchance/cbl:${BASE}-${TAG} nathanchance/cbl:${BASE}
-[[ ${BASE} = "debian" ]] && docker tag nathanchance/cbl:${BASE}-${TAG} nathanchance/cbl:latest
+if [[ ${BASE} = "debian" ]]; then
+    docker tag nathanchance/cbl:${BASE}-${TAG} nathanchance/cbl:latest
+    docker push nathanchance/cbl:latest
+fi
