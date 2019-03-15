3d9563b9e8a4939aadd18c7193688b8ec89560ae
Nathan Chancellor <natechancellor@gmail.com>
2019-03-14 22:06:46 -0700

    Makefile: Support building images with different versions of LLVM/Clang
    Either export or define LLVM_VERSION during make to get a different
    version. We default to the development version (currently 9) because
    that is what CI mostly focuses on.
    
    ex:
    
      $ export LLVM_VERSION=8
      $ make image
    
    or
    
      $ make LLVM_VERSION=9 image
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -eux
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push ${REPO}:${DATE}
-docker push ${REPO}:latest
+docker push ${REPO}:llvm${LLVM_VERSION:=9}-${DATE}
+docker push ${REPO}:llvm${LLVM_VERSION}-latest
+if [[ ${LLVM_VERSION} -eq 9 ]]; then
+    docker push ${REPO}:latest
+fi
