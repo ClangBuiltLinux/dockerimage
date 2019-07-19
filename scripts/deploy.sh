4a5557d111571b2f406ba268adc1fe730d94d598
Nathan Chancellor <natechancellor@gmail.com>
2019-07-18 19:12:19 -0700

    Dockerimage: Update LLVM version
    To mirror https://github.com/llvm/llvm-project/commit/8f5b44aead89a56c6fbf85ccfda03ae1e82ac431
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eux
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push ${REPO}:llvm${LLVM_VERSION:=9}-${DATE}
+docker push ${REPO}:llvm${LLVM_VERSION:=10}-${DATE}
 docker push ${REPO}:llvm${LLVM_VERSION}-latest
-if [[ ${LLVM_VERSION} -eq 9 ]]; then
+if [[ ${LLVM_VERSION} -eq 10 ]]; then
     docker push ${REPO}:latest
 fi
