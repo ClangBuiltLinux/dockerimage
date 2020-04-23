f86952dc2979dab4b78f4bca5e0c00533ed629d5
Harsh Shandilya <me@msfjarvis.dev>
2020-04-23 21:56:45 +0530

    treewide: reformat with shfmt
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- scripts/check-binaries.sh
+++ scripts/check-binaries.sh
@@ -10,19 +10,19 @@ function test_command() {
     fi
 }
 
-BINARIES=( "clang" "ld.lld" "llvm-ar" "llvm-nm" )
+BINARIES=("clang" "ld.lld" "llvm-ar" "llvm-nm")
 for BINARY in "${BINARIES[@]}"; do
     test_command "${BINARY}-${LLVM_VERSION}"
 done
 
-QEMU_SUFFIXES=( "arm" "aarch64" "i386" "mips" "mipsel" "ppc" "ppc64" "x86_64" )
+QEMU_SUFFIXES=("arm" "aarch64" "i386" "mips" "mipsel" "ppc" "ppc64" "x86_64")
 for QEMU_SUFFIX in "${QEMU_SUFFIXES[@]}"; do
     test_command "qemu-system-${QEMU_SUFFIX}"
 done
 
-BINUTILS_PREFIXES=( "" "aarch64-linux-gnu-" "arm-linux-gnueabi-" "mips-linux-gnu-"
-                    "mipsel-linux-gnu-" "powerpc-linux-gnu-" "powerpc64-linux-gnu-"
-                    "powerpc64le-linux-gnu-" "riscv64-linux-gnu-" "s390x-linux-gnu-" )
+BINUTILS_PREFIXES=("" "aarch64-linux-gnu-" "arm-linux-gnueabi-" "mips-linux-gnu-"
+    "mipsel-linux-gnu-" "powerpc-linux-gnu-" "powerpc64-linux-gnu-"
+    "powerpc64le-linux-gnu-" "riscv64-linux-gnu-" "s390x-linux-gnu-")
 for BINUTILS_PREFIX in "${BINUTILS_PREFIXES[@]}"; do
     test_command "${BINUTILS_PREFIX}as"
 done
