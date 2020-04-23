fa1b98d224dd6867549df48e6c7baf0c2457fc0f
Nathan Chancellor <natechancellor@gmail.com>
2020-04-23 10:46:32 -0700

    scripts: Fix shellcheck warnings
    Link: https://github.com/ClangBuiltLinux/dockerimage/actions/runs/85910082
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eux
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push ${REPO}:llvm${LLVM_VERSION:=11}-${DATE}
-docker push ${REPO}:llvm${LLVM_VERSION}-latest
+docker push "${REPO}:llvm${LLVM_VERSION:=11}-${DATE}"
+docker push "${REPO}:llvm${LLVM_VERSION}-latest"
 if [[ ${LLVM_VERSION} -eq 11 ]]; then
-    docker push ${REPO}:latest
+    docker push "${REPO}:latest"
 fi
