1afe7585db24c6f18e9f601bd1c756e83f037c6d
Nathan Chancellor <natechancellor@gmail.com>
2020-07-20 11:50:46 -0700

    dockerimage: Upgrade to LLVM 12
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eux
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push "${REPO}:llvm${LLVM_VERSION:=11}-${DATE}"
+docker push "${REPO}:llvm${LLVM_VERSION:=12}-${DATE}"
 docker push "${REPO}:llvm${LLVM_VERSION}-latest"
-if [[ ${LLVM_VERSION} -eq 11 ]]; then
+if [[ ${LLVM_VERSION} -eq 12 ]]; then
     docker push "${REPO}:latest"
 fi
